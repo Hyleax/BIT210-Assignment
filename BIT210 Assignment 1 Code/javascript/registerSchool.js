86c4f088f9ab3c994192b41ff3b21f3d98e7a5a9
XinYao321 <85153414+XinYao321@users.noreply.github.com>
2022-10-07 23:06:06 +0800

    Update registerSchool.js
    Added comments

--- BIT210 Assignment 1 Code/javascript/registerSchool.js
+++ BIT210 Assignment 1 Code/javascript/registerSchool.js
@@ -1,3 +1,4 @@
+// Declare variables based on their ids
 const form = document.getElementById('form')
 const school = document.getElementById('school')
 const schoolAddress = document.getElementById('school-address')
@@ -5,6 +6,7 @@ const city = document.getElementById('city')
 const button = document.getElementById('register-button')
 const text = document.getElementsByTagName('small')
 
+// button.addEventListener
 button.addEventListener('click', e => {
     e.preventDefault();
     if (validateInput()){
@@ -12,26 +14,29 @@ button.addEventListener('click', e => {
     }
 })
 
-
+// validateInput() function
 const validateInput = () => {
-    if (school.value === "" || schoolAddress.value ==="" 
+    if (school.value === "" || schoolAddress.value ==="" // If user didn't enter anything into the input
     || city.value === ""){
         text[3].textContent = "Please fill up all fields"
         text[3].style.color = "red";
         return false
     }
 
+    // If school length is > 20
     else if(school.value.length > 20) {
         text[0].textContent="School name is too long";
         text[0].style.color = "red";
         return false
 
     }
+    // if school address length is < 10
     else if(schoolAddress.value.length < 10) {
         text[1].textContent = "Invalid school address.";
         text[1].style.color = "red";
         return false
     }
+    // if city length is > 20
     else if(city.value.length > 20) {
         text[2].textContent = "City name is too long";
         text[2].style.color = "red";
@@ -40,7 +45,7 @@ const validateInput = () => {
    
     
 }
-
+// validateButton() function for register button
 const validateButton = () => {
     alert('Registered Successfully');
 }
