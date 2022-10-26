9f37ad481e4e7d630acefc78ccd64292d9e78d74
XinYao321 <85153414+XinYao321@users.noreply.github.com>
2022-10-26 11:34:50 +0800

    Added submitOffer.js

---
+++ BIT210 Assignment 1 Code/javascript/submitOffer.js
@@ -0,0 +1,30 @@
+// Assign variables
+const remark = document.getElementById('remark');
+const text = document.querySelector('small');
+const button = document.getElementById('submit');
+
+// Button event listener
+button.addEventListener('click', (e) =>{
+    e.preventDefault();
+
+    if(validateInput()) {
+        validateButton()
+    }
+
+})
+
+// validateInput() function
+const validateInput = () => {
+    if(remark.value === "") {
+        text.textContent = "Remark cannot be empty!"
+        text.style.color = "red"
+        return false
+    }
+    else {
+        return true
+    }
+}
+// validateButton() function
+const validateButton = () => {
+    alert("Your offer has been submitted.");
+}
