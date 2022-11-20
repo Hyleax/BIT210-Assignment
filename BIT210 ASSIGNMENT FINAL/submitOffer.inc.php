4eaf44bca28cb0cf4e35597d1f6a851c871f81b8
XinYao321 <xinyaolee01@gmail.com>
2022-11-20 22:43:17 +0800

    added request ID and header()

--- BIT210 ASSIGNMENT FINAL/submitOffer.inc.php
+++ BIT210 ASSIGNMENT FINAL/submitOffer.inc.php
@@ -1,19 +1,24 @@
 <?php
 
+// require db_connect.php file
 require 'includes/db_connect.php';
 
 $remark = $_POST['Remark'];
+$requestID = $_POST['requestID'];
 
 // Input validation
 if (empty($remark)) {
-    echo "Input cannot be empty!";
+    header("Location: ../submitOffer.php?erroremptyfields&remark= ".$remark);
+    exit(); // exit
 }
 elseif (strlen($remark) > 200) {
+    header("Location: ../submitOffer.php?error=remarkistoobig&remark".$remark);
+    exit(); // exit
     echo "Your remark is too long";
 }
 
 
-$sqlquery = "INSERT INTO offer(`Remark`, `Offer Status`) VALUES ('$remark', 'PENDING')";
+$sqlquery = "INSERT INTO offer(`offerStatus`, `offerID`, `offerDate`, `remarks`, `volunteerID`, `requestID`) VALUES ('PENDING','$offerID','20-11-2022','$remark','V001','$requestID')";
 
 // If data is inserted into database
 if ($connection->query($sqlquery)) {
